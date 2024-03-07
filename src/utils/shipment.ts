2f031e4a837c9a5a0482074ad417adf9329ffb15
Leon Wang <seasparta618@gmail.com>
2024-03-07 23:23:05 +1100

    Add util to assign color to tracking status

--- src/utils/shipment.ts
+++ src/utils/shipment.ts
@@ -1,4 +1,5 @@
-import { ShipmentStatus } from '~/types/shipment';
+import { theme } from '@chakra-ui/react';
+import { ShipmentStatus, TrackingEventStatus } from '~/types/shipment';
 
 // the util function to assign the tag theme color for status
 export const getShipmentStatusColor = (status: ShipmentStatus): string => {
@@ -13,3 +14,23 @@ export const getShipmentStatusColor = (status: ShipmentStatus): string => {
       return 'green';
   }
 };
+
+// the util function to assign the color fo the tracking status
+export const getTrackingStatusColor = (status: TrackingEventStatus): string => {
+    switch (status) {
+      case 'Picked Up':
+      case 'Arrived at Facility':
+      case 'Processed Through Facility':
+      case 'Departed Facility':
+      case 'On Board for Delivery':
+      case 'Delivered':
+        return theme.colors.green[500];
+      case 'Unknown Scan':
+        return theme.colors.red[500];
+      case 'Return to Sender':
+      case 'Package Handling':
+      default:
+        return theme.colors.yellow[500];
+    }
+  };
+  
