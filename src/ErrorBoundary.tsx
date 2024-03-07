a31b330ba91daedbc1963a7215273160512d5399
Leon Wang <seasparta618@gmail.com>
2024-03-07 21:20:14 +1100

    Push forgot commited ErrorBoundary

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,38 @@
+import React, { ErrorInfo, ReactNode } from 'react';
+import { ErrorContentState } from './components/common/contentStates/ErrorContentState';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorContentState />;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
