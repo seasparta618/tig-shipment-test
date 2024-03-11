53a7eff6d7c7ee3e2fc1dc489be3c2bcd735ddc7
Leon Wang <seasparta618@gmail.com>
2024-03-11 16:15:31 +1100

    Enable code split of dependency in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import viteCompression from 'vite-plugin-compression';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: { outDir: 'build', assetsInlineLimit: 4096, minify:true },
+  plugins: [react(), viteCompression()],
+  build: {
+    outDir: 'build',
+    assetsInlineLimit: 4096,
+    minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id){
+          if(id.includes('node_modules')){
+            return id.split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+        chunkFileNames:'assets/[name]-[hash].js',
+      },
+    },
+  },
   resolve: {
     extensions: ['.js', '.ts', '.tsx', '.json', '.css'],
     alias: {
