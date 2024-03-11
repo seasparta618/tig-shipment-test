c779f9c95bf82f83f093adfd6bd58bcaac6dfdb0
Leon Wang <seasparta618@gmail.com>
2024-03-11 20:11:44 +1100

    Improve vite config, add more ignorable item

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,21 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import viteCompression from 'vite-plugin-compression';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteCompression()],
+  cacheDir: '.vite_cache',
   build: {
     outDir: 'build',
     assetsInlineLimit: 4096,
     minify: true,
     rollupOptions: {
       output: {
-        manualChunks(id){
-          if(id.includes('node_modules')){
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
             return id.split('node_modules/')[1].split('/')[0].toString();
           }
         },
-        chunkFileNames:'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
       },
     },
   },
